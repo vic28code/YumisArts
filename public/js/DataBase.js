3f7e8c6675648cbd8fac0ef63d790767f1e6f876
paul123 <139300207+paul2723@users.noreply.github.com>
2025-06-18 02:59:00 +0000

    cambis

--- public/js/DataBase.js
+++ public/js/DataBase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit } from "firebase/firestore";
 
-// Configuración de tu proyecto Firebase
+// Configuración de tu proyecto Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyBHnihjo2df71IsWSiW-t44Djuxh4l_tRY",
-  authDomain: "pintura2-36966.firebaseapp.com",
-  projectId: "pintura2-36966",
-  storageBucket: "pintura2-36966.firebasestorage.app",
-  messagingSenderId: "496399802898",
-  appId: "1:496399802898:web:198868bd05ad6570e5eecb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Inicializa Firebase y Firestore
